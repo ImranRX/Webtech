0a3f96558b51578d60a8aefe2fa5b8344c884e35
ImranRX <108694329+ImranRX@users.noreply.github.com>
2023-06-25 00:23:54 +0600

    Update profilepic.php

--- lab3/profilepic.php
+++ lab3/profilepic.php
@@ -1,1 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Update Profile</title>
+</head>
+
+<body>
+	<?php
+	$imageFileType = "";
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		// check size limit
+		if (empty($_FILES["upFile"])) {
+			echo "select an image";
+		} elseif (
+			$imageFileType != "jpeg" && $imageFileType != "jpg" && $imageFileType != "png"
+		) {
+			echo "picture foramt must be in jpeg or jpg or png";
+		} elseif ($_FILES["upFile"]["size"] > 40000) {
+			echo "picture size should not be more than 4MB";
+		}
+	}
+	?>
+
+	<div class="proPic">
+		<h2>Profile Picture</h2>
+
+		<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" enctype="multipart/form-data">
+
+			<input type="file" name="upFile" id="upFile">
+			<hr>
+			<input type="submit" value="Submit" name="submit">
+		</form>
+	</div>
+</body>
+
+</html>
